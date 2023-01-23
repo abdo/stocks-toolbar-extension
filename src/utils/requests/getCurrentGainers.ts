8e338869fed6c7cad51b910f262909f15396ef1b
abdo <abdo.mohamadeen@gmail.com>
2023-01-23 17:49:30 +0400

    Update version

--- src/utils/requests/getCurrentGainers.ts
+++ src/utils/requests/getCurrentGainers.ts
@@ -1,6 +1,6 @@
 const getCurrentGainers = () => {
   return fetch(
-    'https://api.polygon.io/v2/snapshot/locale/us/markets/stocks/gainers?apiKey=aGIqQW9gjVprdQlMNXhDB_LQUDmtSicU',
+    `https://api.polygon.io/v2/snapshot/locale/us/markets/stocks/gainers?apiKey=${process.env.polygonApiKey}`,
   )
     .then((response) => response.json());
 };
--- src/utils/requests/getStocksSnapshot.ts
+++ src/utils/requests/getStocksSnapshot.ts
@@ -1,6 +1,6 @@
 const getStocksSnapshot = ({ chosenSymbolsList }: { chosenSymbolsList: string[] }) => {
   return fetch(
-    `https://api.polygon.io/v2/snapshot/locale/us/markets/stocks/tickers?tickers=${chosenSymbolsList.join(',')}&apiKey=aGIqQW9gjVprdQlMNXhDB_LQUDmtSicU`,
+    `https://api.polygon.io/v2/snapshot/locale/us/markets/stocks/tickers?tickers=${chosenSymbolsList.join(',')}&apiKey=${process.env.polygonApiKey}`,
   )
     .then((response) => response.json());
 };
